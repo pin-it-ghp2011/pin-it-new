be5c28f529a9d205b3edba34badfdd70cb44c7a6
yqivy <yqivy.liu@gmail.com>
2021-01-29 17:03:16 -0500

    rename Routes.js and install Puppeteer

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Routes.css';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+function Routes() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Apples
+      </header>
+    </div>
+  );
+}
+
+export default Routes;
