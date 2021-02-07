a4083877d0b1402eddcfa09170d9e58fe2ce11f8
heathernoto <heather.noto@gmail.com>
2021-02-06 21:52:15 -0500

    removed firebase keys

--- extension/popup.js
+++ extension/popup.js
@@ -3,15 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import puppeteer from 'puppeteer';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAab0HdR9spYlqI-0Z3R4QRa3UoYU_Wflc',
-  authDomain: 'pin-it-8dd6e.firebaseapp.com',
-  databaseURL: 'https://pin-it-8dd6e-default-rtdb.firebaseio.com',
-  projectId: 'pin-it-8dd6e',
-  storageBucket: 'pin-it-8dd6e.appspot.com',
-  messagingSenderId: '1054852884997',
-  appId: '1:1054852884997:web:a9126892339a36b5e58a6c',
-};
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
